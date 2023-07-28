6695013d28720d263de0430e0162df63b2207410
jagxas <erdem1umut1iel@gmail.com>
2023-07-28 22:01:12 +0300

    design home

--- src/Components/ContentBox/ContentBox.jsx
+++ src/Components/ContentBox/ContentBox.jsx
@@ -22,7 +22,7 @@ export const ContentBox = (Props) => {
             <div className="text-content" style={{padding: "5px"}}>
                 <h1 className='content-box-title'> {getTranslation(boxTitle)} </h1>
                 {getTranslation(boxContent)}
-                <button className="redirect-button">&gt; More</button>
+                <button className="redirect-button">&gt; {getTranslation("more")}</button>
             </div>
         </div>
     )
@@ -33,24 +33,10 @@ export const ContentBox = (Props) => {
             <div className="text-content" style={{padding: "5px"}}>
                 <h1 className='content-box-title'> {getTranslation(boxTitle)} </h1>
                 {getTranslation(boxContent)}
-                <button className="redirect-button">&gt; More</button>
-            </div>
-            <div className="image-container">
-                <img src={imageAdress} className="image"/>
-            </div>
-        </div>
-    )
-    }
-    else if(type === 2){
-        return (
-        <div className="content-holder-news">
-            <div className="image-container">
-                <img src={imageAdress} className="image"/>
-            </div>
-            <div className="text-content" style={{padding: "5px"}}>
-                <h1 className='content-box-title'> {getTranslation(boxTitle)} </h1>
-                {getTranslation(boxContent)}
                 <button className="redirect-button">&gt; {getTranslation("more")}</button>
+            </div>
+            <div className="image-container">
+                <img src={imageAdress} className="image"/>
             </div>
         </div>
     )
