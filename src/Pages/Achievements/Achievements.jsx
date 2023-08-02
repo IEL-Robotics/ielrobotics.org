51aabefccc188a422db20e6ecfeb4ba04241d48c
jagxas <erdem1umut1iel@gmail.com>
2023-08-02 18:37:02 +0300

    sponsorluk dosyası nerede amk

--- src/Pages/Achievements/Achievements.jsx
+++ src/Pages/Achievements/Achievements.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 
+import "../Achievements/Achievements.css"
+
 import { TheImage } from "../../Components/TheImage/TheImage";
 import { TimelineHolder } from "./Timeline/TimelineHolder";
 import LanguageContext from "../../Context/LanguageContext"
@@ -9,11 +11,42 @@ import success_image from '../../Media/Images/Success.webp'
 export const Achievements = () => {
     const { language, getTranslation } = useContext(LanguageContext);
 
+    let currentSwitch = 1;
+
+    const toggleTo = (toWhich) => {
+        if(currentSwitch != toWhich){
+            currentSwitch = toWhich;
+            
+            if(toWhich === 1){
+                var elementPrev = document.getElementById("btn2");
+                elementPrev.classList.remove("selected");
+
+                var elementNext = document.getElementById("btn1");
+                elementNext.classList.add("selected");
+                console.log("ICERDEYIZ1");
+            }
+            if(toWhich === 2){
+                var elementPrev = document.getElementById("btn1");
+                elementPrev.classList.remove("selected");
+
+                var elementNext = document.getElementById("btn2");
+                elementNext.classList.add("selected");
+                console.log("ICERDEYIZ2");
+            }
+        }
+    }
+
     return(
         <div className="AchievePage" style={{marginTop: "5rem"}}> 
             <TheImage source={success_image} text={getTranslation("success-slogan")}/> 
             <div className='team-title-holder'>
                 <h1 className='team-title'> {getTranslation("achi-title")} </h1>
+            </div>
+            <div className="achi-toggle-container">
+                <div className="achi-slider">
+                    <button className="achi-button selected" id="btn1" onClick={() => toggleTo(1)}>Competetions</button>
+                    <button className="achi-button" id="btn2" onClick={() => toggleTo(2)}>Community</button>
+                </div>
             </div>
             <TimelineHolder/>
         </div>
