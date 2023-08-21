7f31c76a221c60657aff47ae8edd406be5c3f4cd
jagxas <erdem1umut1iel@gmail.com>
2023-08-21 22:31:17 +0300

    tryin sth

--- src/App.jsx
+++ src/App.jsx
@@ -44,9 +44,9 @@ function App() {
       <BR>
         <Navbar handleScroll={handleScroll}/>
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
+          <Route path='/' element={<Achievements/>}></Route>
           <Route path='/sponsors' element={<Sponsors/>}></Route>
-          <Route path='/achievements' element={<Achievements/>}></Route>
+          <Route path='/achievements' element={<Home/>}></Route>
           <Route path='/team' element={<Team/>}></Route>
           <Route path='/news' element={<News/>}></Route>
           <Route></Route>
