96507d677348ce3f01ce8e3d3d35e04c859bacf7
jagxas <erdem1umut1iel@gmail.com>
2024-01-31 14:42:50 +0300

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -5,9 +6,11 @@ import './index.css'
 import LanguageProvider from './LanguageProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <Analytics>
   <LanguageProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
   </LanguageProvider>
+  </Analytics>
 )
